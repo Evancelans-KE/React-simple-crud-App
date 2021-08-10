b98723dfd0e5cc919cbb05da50cafb03287ab9aa
Evance Onyango <evaonyango251@gmail.com>
2021-08-10 16:29:51 +0300

    Included Add and Edit Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+ import react from "react"
+ import { BrowserRouter as Router,Switch,Route } from "react-router-dom";
+ import {Home} from './Components/Home';
+ import {AddUser} from './Components/AddUser';
+ import {EditUser} from './Components/EditUser';
+ import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{maxWidth: "30rem", margin: "4rem auto"}}>
+    <Router>
+      <switch>
+          <Route path="/home"  component={Home} />
+          <Route path="/add" component={AddUser} />
+          <Route path="/edit/:id" component={EditUser } />
+          
+        </switch>
+    </Router>
+      
+         
     </div>
   );
 }
---
+++ src/Components/EditUser.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import {
+       Form,
+       FormGroup,
+       Label,
+       Input,
+       Button
+
+} from 'reactstrap';
+
+export const EditUser = () => {
+    return (
+        <Form>
+          <FormGroup>
+              <label>Country</label>
+              <Input type="text" placeholder="Enter Country"></Input>
+          </FormGroup>
+          <Button type="submit">Edit User</Button>
+          <Link to="/" className="btn btn-danger ml-2">Cancel</Link>
+      </Form>
+    )
+}
